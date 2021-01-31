760dd1eb51e219352850f97e45227440bc9e387a
Shane Daniel <784shane@gmail.com>
2021-01-30 16:14:38 -0800

    add react router

--- site-src-files/src/index.js
+++ site-src-files/src/index.js
@@ -1,5 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import App from './ReactJs/App';
+import About from './ReactJs/About';
+import Contact from './ReactJs/Contact';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
