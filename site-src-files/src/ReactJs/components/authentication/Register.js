3cc0c87ad6860e8863247b589885a435604e7401
BarrySang <barrysang8@gmail.com>
2021-02-03 18:52:32 +0300

    use layout in all existing components

--- site-src-files/src/ReactJs/components/authentication/Register.js
+++ site-src-files/src/ReactJs/components/authentication/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { register } from '../../api_calls/ApiCalls';
+import Layout from '../layouts/Layout';
 
 class Register extends React.Component {
     constructor(props) {
@@ -37,48 +38,56 @@ class Register extends React.Component {
     }
     
     render() {
-        return (
-            <div>
-                <form onSubmit={this.submitHandler}>
-                    <div>
-                        <label htmlFor='fname'>First Name: </label>
-                        <input id='fname' type='text' name='first_name' onChange={this.changehandler}/>
-                    </div>
-                    <div>
-                        <label htmlFor='lname'>Last Name: </label>
-                        <input id='lname' type='text' name='last_name' onChange={this.changehandler}/> <br />
-                    </div>
-                    
-                    <div>
+        const Register = () => {
+            return (
+                <div>
+                    <form onSubmit={this.submitHandler}>
+                        <div>
+                            <label htmlFor='fname'>First Name: </label>
+                            <input id='fname' type='text' name='first_name' onChange={this.changehandler}/>
+                        </div>
+                        <div>
+                            <label htmlFor='lname'>Last Name: </label>
+                            <input id='lname' type='text' name='last_name' onChange={this.changehandler}/> <br />
+                        </div>
+                        
+                        <div>
 
-                    </div>
-                    <label htmlFor='phone'>Phone Number: </label>
-                    <input id='phone' type='text' name='phoneNumber' onChange={this.changehandler}/> <br />
-                    <div>
-                        <label htmlFor='type'>Type: </label>
-                        <input id='type' type='number' name='type' onChange={this.changehandler}/> <br />    
-                    </div>
-                    
-                    <div>
-                        <label htmlFor='email'>Email: </label>
-                        <input id='email' type='text' name='email' onChange={this.changehandler}/> <br />    
-                    </div>
-                    
-                    <div>
-                        <label htmlFor='password'>Password: </label>
-                        <input id='password' type='password' name='password' onChange={this.changehandler}/> <br />    
-                    </div>
-                    
-                    <div>
-                        <label htmlFor='passconf'>Confirm Password: </label>
-                        <input id='passconf' type='password' name='password_confirmation' onChange={this.changehandler}/> <br />    
-                    </div>
-                    
-                    <div>
-                        <input type='submit' value='submit' />    
-                    </div>
-                </form>
-            </div>
+                        </div>
+                        <label htmlFor='phone'>Phone Number: </label>
+                        <input id='phone' type='text' name='phoneNumber' onChange={this.changehandler}/> <br />
+                        <div>
+                            <label htmlFor='type'>Type: </label>
+                            <input id='type' type='number' name='type' onChange={this.changehandler}/> <br />    
+                        </div>
+                        
+                        <div>
+                            <label htmlFor='email'>Email: </label>
+                            <input id='email' type='text' name='email' onChange={this.changehandler}/> <br />    
+                        </div>
+                        
+                        <div>
+                            <label htmlFor='password'>Password: </label>
+                            <input id='password' type='password' name='password' onChange={this.changehandler}/> <br />    
+                        </div>
+                        
+                        <div>
+                            <label htmlFor='passconf'>Confirm Password: </label>
+                            <input id='passconf' type='password' name='password_confirmation' onChange={this.changehandler}/> <br />    
+                        </div>
+                        
+                        <div>
+                            <input type='submit' value='submit' />    
+                        </div>
+                    </form>
+                </div>
+            )
+        }
+
+        return (
+            <Layout Content = {Register}>
+
+            </Layout>
         )
     }
 }
--- site-src-files/src/ReactJs/components/layouts/partial/Header.js
+++ site-src-files/src/ReactJs/components/layouts/partial/Header.js
@@ -5,6 +5,10 @@ class Header extends React.Component {
         return (
             <div>
                 <h1>Header</h1>
+                <p><a href="/profile">Profile</a></p>
+                <p><a href="/register">Register</a></p>
+                <p><a href="/login">Login</a></p>
+                <p><a href="/">Home</a></p>
             </div>
         )
     }
