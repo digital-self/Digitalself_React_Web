1deb814c25650c81c3360b78b6ce7c0eed7ff38d
BarrySang <barrysang8@gmail.com>
2021-02-04 14:03:20 +0300

    show relevant links depending on state of user's authentiction

---
+++ site-src-files/src/ReactJs/components/layouts/partial/AuthenticatedComponents.js
@@ -0,0 +1,10 @@
+const AuthenticatedComponents = () => {
+    return (
+        <div>
+            <p><a href="/profile">Profile</a></p>
+            <p><a href="/">Home</a></p>
+        </div>
+    )
+}
+
+export default AuthenticatedComponents;
---
+++ site-src-files/src/ReactJs/components/layouts/partial/UnauthenticatedComponents.js
@@ -0,0 +1,10 @@
+const AuthComponents = () => {
+    return (
+        <div>
+            <p><a href="/register">Register</a></p>
+            <p><a href="/login">Login</a></p>    
+        </div>
+    )
+}
+
+export default AuthComponents;
