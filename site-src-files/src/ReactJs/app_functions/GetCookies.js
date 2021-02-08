6907d9a9c7487fe78fc3281d59fc447f2df41cb6
BarrySang <barrysang8@gmail.com>
2021-02-08 14:34:07 +0300

    finih accepting change from remote repo

--- site-src-files/src/ReactJs/app_functions/GetCookies.js
+++ site-src-files/src/ReactJs/app_functions/GetCookies.js
@@ -1,15 +1,5 @@
 import Cookies from 'js-cookie';
 
-<<<<<<< HEAD
-export function getUserId () {
-    console.log(Cookies.get('userid'));
-    return Cookies.get('userid');
-}
-
-export function getToken () {
-    return Cookies.get('token');
-}
-=======
 export function getUserId() {
     return Cookies.get('userid') || [];
 }
@@ -17,4 +7,3 @@ export function getUserId() {
 export function getToken() {
     return Cookies.get('token') || "";
 }
->>>>>>> f4fd94965c1206e0d148883d11b307175f591b2e
--- site-src-files/src/ReactJs/components/layouts/partial/Header.js
+++ site-src-files/src/ReactJs/components/layouts/partial/Header.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import { checkAuth } from '../../../app_functions/CheckAuth';
-=======
 import { Auth } from '../../../app_functions/Auth';
->>>>>>> f4fd94965c1206e0d148883d11b307175f591b2e
 import LinksContainer from '../partial/LinksContainer';
 import AuthenticatedComponents from './AuthenticatedComponents';
 import AuthComponents from './UnauthenticatedComponents';
@@ -11,13 +7,7 @@ import AuthComponents from './UnauthenticatedComponents';
 class Header extends React.Component {
     render() {
         let Contents;
-<<<<<<< HEAD
-        
-        
-        if(!checkAuth()) {
-=======
         if(!Auth().isLoggedIn()) {
->>>>>>> f4fd94965c1206e0d148883d11b307175f591b2e
             Contents = () => {
                 return (
                     <div>
@@ -34,11 +24,6 @@ class Header extends React.Component {
                 )
             }
         }
-<<<<<<< HEAD
-        
-=======
-
->>>>>>> f4fd94965c1206e0d148883d11b307175f591b2e
         return (
             <div>
                 <h1>Header</h1>
@@ -49,8 +34,4 @@ class Header extends React.Component {
     }
 }
 
-<<<<<<< HEAD
 export default Header;
-=======
-export default Header;
->>>>>>> f4fd94965c1206e0d148883d11b307175f591b2e
